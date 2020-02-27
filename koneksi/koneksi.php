f9cf2c7f6691283d94b15f074d2aa0071edcc821
Indra Mochamad Farros <43379139+indrafarros@users.noreply.github.com>
2020-02-27 10:31:31 +0700

    Add files via upload

---
+++ koneksi/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "db_umkm";
+ 
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if(mysqli_connect_errno()){
+	echo"<script> alert('Koneksi database gagal.!!'); </script>";
+}
+?>
